51b83a2912faf0b163bb927ca73e92ce58b28b3c
rokibulislam <rrcucse.11@gmail.com>
2022-07-23 09:56:35 +0600

    add completed line

--- src/components/todos/todo/index.jsx
+++ src/components/todos/todo/index.jsx
@@ -5,7 +5,7 @@ import './styles.scss'
 const Todo = ({ item, key, handleComplete, handleDeleteItem }) => {
   return (
     <li className="todo-item" key={key}> 
-        <span className="todo-title"> {item.title}  </span>
+        <span className="todo-title"style={{ textDecoration: item.completed ? "line-through" : "" }}> {item.title}  </span>
         <input type="checkbox" onChange={ (e) => handleComplete(e, item)} checked={item.completed} />
         <Button onClick={ (e) => handleDeleteItem(e, item.id)} label="Delete" variant="danger">  </Button>
     </li>
